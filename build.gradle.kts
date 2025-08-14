1f77b6a5f6f2c3c55a48b160f34eaf578fe8b423
zzsyppt <57822569+zzsyppt@users.noreply.github.com>
2025-08-14 18:47:33 +0800

    build(kotlin): bump Kotlin plugin to 1.9.25 to match Compose compiler 1.5.15

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    // 把 Kotlin 插件版本改为 1.9.25
+    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
 }
